73e1ea4155a4f8bbcd29c5b1b6d25a3a512f5573
dang <doanhaidang32@gmail.com>
2017-02-08 14:18:13 +0700

    Add new file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206155557) do
+ActiveRecord::Schema.define(version: 20170208071238) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:'Dang1',email:'Dang1@gmail.com')
+User.create!(name:'Dang2',email:'Dang2@gmail.com')
+User.create!(name:'Dang3',email:'Dang3@gmail.com')
+User.create!(name:'Dang4',email:'Dang4@gmail.com')
